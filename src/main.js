f4057472675b9ef9a7debec254d236e1208612f9
sarai gonzalez <saraiglezyglez@gmail.com>
2022-03-08 22:56:43 -0600

    terminado css de creacion de cuenta y welcome back

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js';
-import { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, GithubAuthProvider } from 'https://www.gstatic.com/firebasejs/9.6.6/firebase-auth.js';
 import { errorArea, showSignUpError } from './ui.js';
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -47,16 +47,14 @@ const auth = getAuth(app);
 // sign up for users
 const signUpForm = document.querySelector('#signUpForm');
 const txtEmail = document.getElementById('txtEmail');
-const txtUsername = document.getElementById('txtUsername');
 const txtPassword = document.getElementById('txtPassword');
 
 const createAccount = async () => {
   const email = txtEmail.value;
-  const username = txtUsername.value;
   const password = txtPassword.value;
 
   try {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, username, password);
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
     console.log(userCredential.user);
     errorArea.innerHTML = '';
     signUpForm.reset();
@@ -69,56 +67,104 @@ const createAccount = async () => {
 const btnSignUp = document.getElementById('btn-signUp');
 btnSignUp.addEventListener('click', createAccount);
 
-
-//sign in with google
+//  sign in with google
 
 const googleProvider = new GoogleAuthProvider();
 
 const btnGoogle = document.getElementById('btn-google');
-btnGoogle.addEventListener("click", () => { 
+btnGoogle.addEventListener('click', () => { 
   signInWithPopup(auth, googleProvider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    // The signed-in user info.
-    const user = result.user;
-    console.log(user);
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      // The signed-in user info.
+      const user = result.user;
+      console.log(user);
     // ...
-  }).catch((error) => {
+    }).catch((error) => {
     // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
     // ...
-  });
-})
+    });
+});
+
+//  sign in with google in welcome back page
 
 
-//sign in with facebook
-
-const facebookProvider = new FacebookAuthProvider();
-
-const btnFacebook = document.getElementById('btn-facebook');
-btnFacebook.addEventListener('click', () => {
-  signInWithPopup(auth, facebookProvider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = FacebookAuthProvider.credentialFromResult(result);
-    // The signed-in user info.
-    const user = result.user;
-    console.log(user);
+const btnGoogle2 = document.getElementById('btn-google2');
+btnGoogle2.addEventListener('click', () => { 
+  signInWithPopup(auth, googleProvider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      // The signed-in user info.
+      const user = result.user;
+      console.log(user);
     // ...
-  }).catch((error) => {
+    }).catch((error) => {
     // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.email;
-    // The AuthCredential type that was used.
-    const credential = FacebookAuthProvider.credentialFromError(error);
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
     // ...
-  });
-})
+    });
+});
+
+//  sign in with github
+
+const githubProvider = new GithubAuthProvider();
+
+const btnGithub = document.getElementById('btn-github');
+btnGithub.addEventListener('click', () => {
+  signInWithPopup(auth, githubProvider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GithubAuthProvider.credentialFromResult(result);
+      // The signed-in user info.
+      const user = result.user;
+      console.log(user);
+    // ...
+    }).catch((error) => {
+    // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GithubAuthProvider.credentialFromError(error);
+    // ...
+    });
+});
+
+//  sign in with github in welcome back page
+
+const btnGithub2 = document.getElementById('btn-github2');
+btnGithub2.addEventListener('click', () => {
+  signInWithPopup(auth, githubProvider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GithubAuthProvider.credentialFromResult(result);
+      // The signed-in user info.
+      const user = result.user;
+      console.log(user);
+    // ...
+    }).catch((error) => {
+    // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GithubAuthProvider.credentialFromError(error);
+    // ...
+    });
+});
+
