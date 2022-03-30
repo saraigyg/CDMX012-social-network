4be63d2b36717c3127581eff47bad65f12a5912d
sarai gonzalez <saraiglezyglez@gmail.com>
2022-03-29 20:39:10 -0600

    se agregaron funciones a like.spec.js y genericModal.spec.js

--- test/like.spec.js
+++ test/like.spec.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 import { like } from '../src/components/like.js';
+import { giveLike, dislike } from '../src/lib/likes.js';
 
 jest.mock('../src/firebase-imports.js');
 
@@ -12,5 +13,35 @@ describe('Tests of like', () => {
     const rootDiv = document.getElementById('root');
     rootDiv.appendChild(renderlike);
     expect(rootDiv.innerHTML).toMatchSnapshot();
+  });
+  test('cuando click es true', () => {
+    const elements = [true, 2, 'saraiDos', 'sarai12', ['mario', 'charlotte']];
+    const div = like(...elements);
+    document.body.innerHTML = '<div id="root"></div>';
+    const rootDiv = document.getElementById('root');
+    rootDiv.appendChild(div);
+    expect(rootDiv.innerHTML).toMatchSnapshot();
+  });
+  test('cuando click es false', () => {
+    const elements = [false, 2, 'saraiDos', 'sarai12', ['mario', 'charlotte']];
+    const div = like(...elements);
+    document.body.innerHTML = '<div id="root"></div>';
+    const rootDiv = document.getElementById('root');
+    rootDiv.appendChild(div);
+    expect(rootDiv.innerHTML.includes('./assets/like.png')).toBe(true);
+  });
+});
+
+describe('the function giveLike modify the list of likes', () => {
+  test('render a modified list', () => {
+    const elements = ['charles', 'charles2022', ['maria', 'benny']];
+    giveLike(...elements);
+  });
+});
+
+describe('the function dislike modify the list of likes', () => {
+  test('render a modified list', () => {
+    const elements = ['charles', 'charles2022', ['maria', 'benny']];
+    dislike(...elements);
   });
 });
