c05b8a1730b626ba8f2e218b114d608d6bdf0ba2
Perla Del Ángel <perdelangel@gmail.com>
2022-04-01 17:47:29 -0600

    Tests que evaluan la ejecución correcta de los botones asociados a la página de sign-up

--- test/sign-up.spec.js
+++ test/sign-up.spec.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 import { signUpPage } from '../src/pages/sign-up.js';
+import { signInWithPopup } from '../src/firebase-imports.js';
 
 jest.mock('../src/firebase-imports.js');
 
@@ -12,5 +13,41 @@ describe('Tests of signUpPage', () => {
     const rootDiv = document.getElementById('root');
     rootDiv.appendChild(renderSignUp);
     expect(rootDiv.innerHTML).toMatchSnapshot();
+  });
+
+  test('should render the sign-up form and append it ', () => {
+    const renderSignUp = signUpPage();
+    document.body.innerHTML = '<div id="root"></div>';
+    const rootDiv = document.getElementById('root');
+    rootDiv.appendChild(renderSignUp);
+
+    const signUpForm = document.querySelector('.createAccount-container') != null;
+    expect(signUpForm).toBe(true);
+  });
+
+  test('should call the sign-up with Google function', async () => {
+    const googleBtn = document.querySelector('#btn-google');
+    googleBtn.dispatchEvent(new Event('click'));
+    await signInWithPopup.mockResolvedValue();
+
+    expect(window.location.pathname).toBe('/home');
+  });
+
+  test('should call the sign-up with Facebook function', async () => {
+    document.body.innerHTML = '<div id="root"><input type="button" id="btn-facebook"></div>';
+    const facebookBtn = document.querySelector('#btn-facebook');
+    facebookBtn.dispatchEvent(new Event('click'));
+    await signInWithPopup.mockResolvedValue();
+
+    expect(window.location.pathname).toBe('/home');
+  });
+
+  test('should call the sign-up with GitHub function', async () => {
+    document.body.innerHTML = '<div id="root"><input type="button" id="btn-github"></div>';
+    const githubBtn = document.querySelector('#btn-github');
+    githubBtn.dispatchEvent(new Event('click'));
+    await signInWithPopup.mockResolvedValue();
+
+    expect(window.location.pathname).toBe('/home');
   });
 });
