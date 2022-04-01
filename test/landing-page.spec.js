7cae4537e04538a7850b59e2dda337612fd8fbbd
Sarai Rojas <sara.r.sandoval@gmail.com>
2022-04-01 11:05:25 -0600

    Test sign in ans sign up btns of landing page

--- test/landing-page.spec.js
+++ test/landing-page.spec.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 import { landingPage } from '../src/pages/landing-page.js';
+// import { onNavigate } from '../src/app.js';
 
 jest.mock('../src/firebase-imports.js');
 
@@ -11,6 +12,23 @@ describe('Tests  of landingPage', () => {
     document.body.innerHTML = '<div id="root"></div>';
     const rootDiv = document.getElementById('root');
     rootDiv.appendChild(renderLanding);
+    console.log(document.body.innerHTML);
     expect(rootDiv.innerHTML).toMatchSnapshot();
+  });
+  test('should render sign-up page', () => {
+    const signUpBtn = document.querySelector('.btn-signUp');
+    signUpBtn.dispatchEvent(new Event('click'));
+    expect(window.location.pathname).toBe('/sign-up');
+  });
+  test('should render sign-in page', () => {
+    const rootDiv = document.getElementById('root');
+    while (rootDiv.firstChild) {
+      rootDiv.removeChild(rootDiv.firstChild);
+    }
+    const renderLanding = landingPage();
+    rootDiv.appendChild(renderLanding);
+    const signInBtn = document.querySelector('.btn-signIn');
+    signInBtn.dispatchEvent(new Event('click'));
+    expect(window.location.pathname).toBe('/sign-in');
   });
 });
