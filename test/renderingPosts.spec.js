09440b2d92f7a47b6be6752dbce729f1ed23552d
Perla Del Ángel <perdelangel@gmail.com>
2022-04-01 21:48:29 -0600

    Tests para renderingPosts.js con 100% de covertura

--- test/renderingPosts.spec.js
+++ test/renderingPosts.spec.js
@@ -2,28 +2,113 @@
  * @jest-environment jsdom
  */
 import { createPosts } from '../src/components/renderingPosts.js';
-// import { onNavigate } from '../src/app.js';
+/* import { genericModal } from '../src/components/genericModal.js'; */
 
 jest.mock('../src/firebase-imports.js');
 
-describe('Tests the creation of a post', () => {
-  test('render post', () => {postData, currentUid, name, username
+describe('Rendering posts:', () => {
+  test('a new post is created and properly rendered', () => {
     const postData = {
       date: '01/04/2022',
       reading: 'Test title',
       text: 'Great book!',
-      key: '1234',
+      key: '123456789',
       likes: [],
       uid: 'userUid',
-    }
-
+    };
     const name = 'Eli';
     const username = 'DinoEli';
     const currentUid = 'abc1234';
-    
+
     document.body.innerHTML = '<div id="postsArea"></div>';
-    const post = createPosts();
-    
-    expect(rootDiv.innerHTML).toMatchSnapshot();
+    createPosts(postData, currentUid, name, username);
+    const postArea = document.querySelector('#postsArea');
+
+    expect(postArea.innerHTML).toMatchSnapshot();
+  });
+
+  test('Renders like component of a post that the user has liked before', () => {
+    const postData = {
+      date: '01/04/2022',
+      reading: 'Test title',
+      text: 'Great book!',
+      key: '123456789',
+      likes: ['saraiG'],
+      uid: 'userUid',
+    };
+    const name = 'Eli';
+    const username = 'DinoEli';
+    const currentUid = 'saraiG';
+
+    document.body.innerHTML = '<div id="postsArea"></div>';
+    createPosts(postData, currentUid, name, username);
+    const postArea = document.querySelector('#postsArea');
+
+    expect(postArea.innerHTML.includes('./assets/heart.png')).toBe(true);
+  });
+
+  test('Dropdown menu shows on click', () => {
+    const postData = {
+      date: '01/04/2022',
+      reading: 'Test title',
+      text: 'Great book!',
+      key: '123456789',
+      likes: [],
+      uid: 'saraiG',
+    };
+    const name = 'Eli';
+    const username = 'DinoEli';
+    const currentUid = 'saraiG';
+
+    document.body.innerHTML = '<div id="postsArea"></div>';
+    createPosts(postData, currentUid, name, username);
+    const optionsMenu = document.querySelector('.options-menu');
+    optionsMenu.dispatchEvent(new Event('click'));
+
+    const postArea = document.querySelector('#postsArea');
+
+    expect(postArea.innerHTML).toMatchSnapshot();
+  });
+
+  test('Modal shows up after click on delete button', () => {
+    const postData = {
+      date: '01/04/2022',
+      reading: 'Test title',
+      text: 'Great book!',
+      key: '123456789',
+      likes: [],
+      uid: 'saraiG',
+    };
+    const name = 'Eli';
+    const username = 'DinoEli';
+    const currentUid = 'saraiG';
+
+    document.body.innerHTML = '<div id="postsArea"></div>';
+    createPosts(postData, currentUid, name, username);
+    const deleteOption = document.querySelector('.deletePost');
+    deleteOption.dispatchEvent(new Event('click'));
+
+    expect(document.body.innerHTML).toMatchSnapshot();
+  });
+
+  test('Edit function is executed properly on click', () => {
+    const postData = {
+      date: '01/04/2022',
+      reading: 'Test title',
+      text: 'Great book!',
+      key: '123456789',
+      likes: [],
+      uid: 'saraiG',
+    };
+    const name = 'Eli';
+    const username = 'DinoEli';
+    const currentUid = 'saraiG';
+
+    document.body.innerHTML = '<div id="postsArea"></div>';
+    createPosts(postData, currentUid, name, username);
+    const editOption = document.querySelector('.editPost');
+    editOption.dispatchEvent(new Event('click'));
+
+    expect(document.body.innerHTML).toMatchSnapshot();
   });
 });
